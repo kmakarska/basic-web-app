f9dcdffa1588b6d74bd40b86ffa51dc7152ff80b
Katie Makarska <katie.makarska@gmail.com>
2025-02-25 10:32:57 -0500

    add 3

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -18,8 +18,8 @@ export default function QueryProcessor(query: string): string {
 
   if (query.toLowerCase().includes("plus")) {
     const numbers = query.match(/\d+/g)?.map(Number) || [];
-    if (numbers.length === 2) {
-      return (numbers[0] + numbers[1]).toString();
+    if (numbers.length > 0) {
+      return numbers.reduce((sum, num) => sum + num, 0).toString();
     }
   }
 
